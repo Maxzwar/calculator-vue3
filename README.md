ca288f092b58a3a01364033f72ca907e1296a376
Ohrworm Dev <ohrworm@Ohrworms-MacBook-Pro.local>
2021-01-24 05:07:25 -0500

    webpack - css

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # calculator-vue3
 calculator frontend-only app
+
+https://stackoverflow.com/questions/64868632/vuejs-3-problem-with-vue-template-compiler
+
+https://lmiller1990.github.io/electic/posts/20200406_webpack_for_vue_3.html
+
+testing
+https://vueschool.io/lessons/installing-vue-test-utils
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path')
+
+const { VueLoaderPlugin } = require('vue-loader')
+
+module.exports = {
+  mode: 'development',
+  entry: './src/main.js',
+  output: {
+    path: path.resolve(__dirname, './dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'vue-style-loader',
+          'css-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new VueLoaderPlugin()
+  ],
+  devServer: {
+    inline: true,
+    hot: true,
+    stats: 'minimal',
+    contentBase: __dirname,
+    overlay: true
+  }
+}
